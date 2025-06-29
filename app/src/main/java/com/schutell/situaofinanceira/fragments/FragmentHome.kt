178344f6753a567b6165bb579a1a86d98b11b25d
SantiagoSchutell <contatosantiagoschutell@gmail.com>
2025-06-29 20:45:26 -0300

    Inplementado o FireBase no projeto mais a criação das telas de login e cadastro de usuario

--- app/src/main/java/com/schutell/situaofinanceira/fragments/FragmentHome.kt
+++ app/src/main/java/com/schutell/situaofinanceira/fragments/FragmentHome.kt
@@ -1,3 +1,46 @@
 package com.schutell.situaofinanceira.fragments
 
-data class FragmentHome()
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.google.firebase.auth.FirebaseAuth
+import com.schutell.situaofinanceira.R
+import com.schutell.situaofinanceira.databinding.ActivityHomeBinding
+
+class FragmentHome: Fragment(){
+
+     private var _binding: ActivityHomeBinding? = null
+    private val binding get() = _binding!!
+
+     override fun onCreateView(
+         inflater: LayoutInflater,
+         container: ViewGroup?,
+         savedInstanceState: Bundle?
+     ): View? {
+         _binding = ActivityHomeBinding.inflate(inflater, container, false)
+         return binding.root
+     }
+    private fun verificarLogin(){
+        val usuario = FirebaseAuth.getInstance().currentUser
+
+        if (usuario != null){
+            findNavController().navigate(R.id.action_homepage)
+        } else{
+
+            findNavController().navigate(R.id.fragmentLogin)
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSair.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            verificarLogin()
+
+        }
+    }
+}
